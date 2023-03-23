6b8f38ee4b4e93e3afe89e9c5d0ad9e6d5f15386
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-03-23 20:46:28 -0300

    Add dependency injection example on HelloWorldController

--- webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/controller/HelloWorldController.java
@@ -1,12 +1,25 @@
 package ar.edu.itba.paw.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 
 
 @Controller
 public class HelloWorldController {
+
+    // This field and the following constructor are an example on how to use the dependency injection engine.
+    // The HelloWorldController tells Spring that it wants a ViewResolver, from a method called "viewResolver".
+    // Spring gets that from the @Bean method in WebConfig.java and passes it to here.
+    private final ViewResolver viewResolver;
+    @Autowired
+    public HelloWorldController(@Qualifier("viewResolver") final ViewResolver vr) {
+        this.viewResolver = vr;
+    }
 
     // El RequestMapping se puede configurar para solo funcionar si el request tiene tal header, o es tal método HTTP,
     // o qué produce, etc.
