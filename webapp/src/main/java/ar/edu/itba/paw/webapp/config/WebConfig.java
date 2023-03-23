e1b1b953713bbb591d0753bebc9477fbcf0b3ddb
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-03-23 20:38:15 -0300

    Add additional comments to WebConfig.java viewResolver()

--- webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
+++ webapp/src/main/java/ar/edu/itba/paw/webapp/config/WebConfig.java
@@ -12,12 +12,16 @@ import org.springframework.web.servlet.view.JstlView;
 @ComponentScan({"ar.edu.itba.paw.webapp.controller"})
 @Configuration
 public class WebConfig {
+
+    // Con @Bean, yo básicamente le digo a sprint "Che, si alguien te pide un ViewResolver, en vez de darle el default,
+    // dale este. Por default, genera una sola instancia y siempre se le da a todos esa misma instancia.
+    // Yo no se qué clase va a usar este ViewResolver, y dicha clase tampoco sabe de dónde salió ese ViewResolver.
     @Bean
     public ViewResolver viewResolver() {
-        final InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/jsp/");
-        viewResolver.setSuffix(".jsp");
-        return viewResolver;
+        final InternalResourceViewResolver vr = new InternalResourceViewResolver();
+        vr.setViewClass(JstlView.class);
+        vr.setPrefix("WEB-INF/jsp/");
+        vr.setSuffix(".jsp");
+        return vr;
     }
 }
