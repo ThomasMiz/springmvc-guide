509febcc3938513146ed03a9e4b6ff46b48dc73b
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2023-03-26 13:07:46 -0300

    Create projects for models, interfaces, persistence, services

---
+++ persistence/src/main/java/ar/edu/itba/paw/persistence/App.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.paw.persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ services/src/main/java/ar/edu/itba/paw/services/App.java
@@ -0,0 +1,13 @@
+package ar.edu.itba.paw.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
